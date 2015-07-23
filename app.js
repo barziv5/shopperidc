adc030d86bf8cb0808e67d5a03c4cc2951836735
barziv5 <barziv4@gmail.com>
2015-07-23 15:18:03 +0300

    package.json

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(80, function () {
+var port = proccess.env.PORT || 3000;
+var server = app.listen(port, function () {
     var host = server.address().address;
-    var port = server.address().port;
+
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
