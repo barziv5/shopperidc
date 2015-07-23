06457afc6b8642ffb05439bdb0099fd09bd941df
barziv5 <barziv4@gmail.com>
2015-07-23 17:28:31 +0300

    mongoaccesslayer

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
  * Created by ziv on 23/7/2015.
  */
 var express = require('express');
+var mongoaccesslayer = require('./mongoAccesslayer.js');
 var app = express();
 
 app.use(express.static(__dirname + '/web'));
 var port = process.env.PORT || 3000;
 var server = app.listen(port);
 
-documnet =
+var documnet = {
+    "FirstName": "test3",
+    "LastName": "test3",
+    "email": "test3@gmail.com",
+    "UserName": "test3",
+    "Password": "1232",
+    "UserId": "12345678902",
+    "Hobbies": [
+        "Sport",
+        "tech"
+    ]
+}
+
+function insert(collectionName,document){
+    mongoaccesslayer.insertDocument('users',document,function(err,data){
+        console.log(err);
+    })
+}
 
 /*console.log('start');
 
