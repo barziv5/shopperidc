67eff47d43b5c57896ee3aa024758b817b2393df
barziv5 <barziv4@gmail.com>
2015-07-23 15:27:06 +0300

    index

--- app.js
+++ app.js
@@ -4,14 +4,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-var port = proccess.env.PORT || 3000;
-var server = app.listen(port, function () {
-    var host = server.address().address;
-
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+app.use(express.static(__dirname + '/web'));
+var port = process.env.PORT || 3000;
+var server = app.listen(port);
